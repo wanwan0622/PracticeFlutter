75eaa00e2ad816e00a29e41016d21f9c789d4ad8
wanwan0622 <minnie20000622@yahoo.co.jp>
2020-10-26 22:41:13 +0900

    Statefulウィジェット

--- practice_flutter/lib/main.dart
+++ practice_flutter/lib/main.dart
@@ -17,9 +17,22 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
